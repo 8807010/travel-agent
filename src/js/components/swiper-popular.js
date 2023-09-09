05f68e93e6afa34c41df7a6e7ba970617eabd70e
Artiom_Sokolov <sokolova-net@mail.ru>
2023-09-09 09:28:25 +0300

    media querries for section popular

--- src/js/components/swiper-popular.js
+++ src/js/components/swiper-popular.js
@@ -1,11 +1,21 @@
 import Swiper from 'swiper/bundle';
 
 const swiper = new Swiper('#swiper-popular', {
-  slidesPerView: 4,
+  slidesPerView: 1,
   spaceBetween: 32,
   navigation: {
     nextEl: '#popularNext',
     prevEl: '#popularPrev',
+  },
+  breakpoints: {
+    425: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 32,
+    }
   },
 });
 
