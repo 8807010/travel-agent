f318064ae24d7f07ce0ecb7de3c57e9df930a200
Artiom_Sokolov <sokolova-net@mail.ru>
2023-09-12 18:37:17 +0300

    Update 2023-09-12T15:36:59.504Z

--- src/js/_components.js
+++
@@ -1,6 +0,0 @@
-import './components/easePick';
-import './components/autoComplete';
-import './components/swiper-popular';
-import './components/swiper-testimonials';
-import './components/scrollReveal';
-import './components/preloader';
