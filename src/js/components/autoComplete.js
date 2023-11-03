88f7f637b2579bde972a5af32a4bd0a97038b3a5
Artiom_Sokolov <sokolova-net@mail.ru>
2023-11-03 14:25:44 +0300

    new cities for autocomlite plagin

--- src/js/components/autoComplete.js
+++ src/js/components/autoComplete.js
@@ -1,11 +1,26 @@
 import autoComplete from "@tarekraafat/autocomplete.js";
-import countryList from './../vendor/countryList';
+import countryList from './../../data/countries.min.json';
+
+const func = () => {
+  const res = [];
+  for (let country in countryList) {
+    const cities = countryList[country];
+    cities.forEach((city) => {
+      const str = `${city}, ${country}`;
+      console.log(str)
+      res.push(str);
+    });
+  };
+  return res;
+};
+
+const countries = func();
 
 const autoCompleteJS = new autoComplete({
   selector: '#autoCompleteCountry',
   placeHolder: 'e.g Bali, Indonesia',
   data: {
-    src: countryList,
+    src: countries,
     cache: true,
   },
   resultItem: {
